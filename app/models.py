6c982599a5b3d0529a161fbadab6e84423ca91f7
Gichimu <gichimueric12@gmail.com>
2019-12-01 10:40:17 +0300

    create the users, comments and blogposts models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 class Quote:
     '''
@@ -10,16 +12,61 @@ class Quote:
         self.quote = quote
 
 
+class User:
+    '''
+    Class that defines the user object template
+    '''
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    email = db.Column(db.String(80), unique=True, index=True)
+    blogposts = db.relationship('Blogpost', backref='users', lazy=True)
+    password_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute!')
+
+    @password.setter
+    def password(self, password):
+        self.password_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_secure, password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
 class Comment:
     '''
     Class that defines the default template for the comment object
     '''
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    blog_id = db.Column(db.Integer, db.ForeignKey('blogposts.id'))
+    comment = db.Column(db.String())
+    name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique = True, index = True)
 
-    def __init__(self, id, comment, name, email):
-        self.id = id
-        self.comment = comment
-        self.name = name
-        self.email = email
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+class BlogPost:
+    '''
+    Class that defines the default template for the blog object
+    '''
+
+    __tablename__ = 'blogposts'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    photo_uri = db.Column(db.String(255))
+    blog_text = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime)
+    comments = db.relationship('Comment', backref='blogpost', lazy=True)
+
+    
 
     
 
